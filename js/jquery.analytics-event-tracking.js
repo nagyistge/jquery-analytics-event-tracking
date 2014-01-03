512172ab2a1d980f8998e3375bf0d9b29f7f2cbf
Tommy Chan <tochan@contractor.ea.com>
2014-01-03 08:59:21 -0800

    Fixed issue where interaction was not defined. Unified formatting (it was using spaces in some places before and tabs elsewhere).

--- js/jquery.analytics-event-tracking.js
+++ js/jquery.analytics-event-tracking.js
@@ -5,84 +5,70 @@
  * @author        Pascal van Gemert
  * @copyright     (c) 2013 Pascal van Gemert
  * Usage Command Line: $('body').analyticsEventTracking()
- * @version 0.1.0
+ * @version 0.1.1
  * @requires jQuery 1.7+
  */
 
-(function($) 
-{
-    $.fn.analyticsEventTracking = function(options) 
-    {
-        var settings = $.extend({
-            selector:			'.track',
-            default_category: 	'General'
-        }, options);
+(function ($) {
+	$.fn.analyticsEventTracking = function (options) {
+		var settings = $.extend({
+			selector: '.track',
+			default_category: 'General',
+			action: 'click'
+		}, options);
 
-        if(typeof ga !== 'function')
-    	{
-    		log('Google Analytics (analytics.js) is not initialized.');
-    		return false;
-    	}
+		if (typeof ga !== 'function') {
+			log('Google Analytics (analytics.js) is not initialized.');
+			return false;
+		}
 
-        return $(this).each(function() 
-        {
-        	var _self = $(this);
+		return $(this).each(function () {
+			var _self = $(this);
 
-        	bindEvents();
+			bindEvents();
 
-            function bindEvents()
-            {
-				_self.find(settings.selector).on('click', function(e)
-				{
+			function bindEvents() {
+				_self.find(settings.selector).on('click', function (e) {
 					trackEvent('click', $(this));
 				});
 
-				_self.find(settings.selector + '-blur').on('blur', function(e)
-				{
+				_self.find(settings.selector + '-blur').on('blur', function (e) {
 					trackEvent('blur', $(this));
 				});
 
-				_self.find(settings.selector + '-complete').on('blur', function(e)
-				{
-					if($.trim($(this).val()) != '')
-					{
+				_self.find(settings.selector + '-complete').on('blur', function (e) {
+					if ($.trim($(this).val()) != '') {
 						trackEvent('complete', $(this));
 					}
 				});
 
-				_self.find(settings.selector + '-focus').on('focus', function(e)
-				{
+				_self.find(settings.selector + '-focus').on('focus', function (e) {
 					trackEvent('focus', $(this));
 				});
 
-				_self.find(settings.selector + '-mouseover').on('mouseover', function(e)
-				{
+				_self.find(settings.selector + '-mouseover').on('mouseover', function (e) {
 					trackEvent('mouseover', $(this));
 				});
 
-				_self.find(settings.selector + '-change').on('change', function(e)
-				{
+				_self.find(settings.selector + '-change').on('change', function (e) {
 					trackEvent('change', $(this));
 				});
 			};
 
-			function trackEvent(event_type, element)
-			{
-				var category 	= element.data('category') || settings.default_category;
-				var action 		= element.data('action') || interaction;
-				var label 		= element.data('label') || ''; // optional
-				var value 		= element.data('value') || 1; // optional
+			function trackEvent(event_type, element) {
+				var category = element.data('category') || settings.default_category;
+				var action = element.data('action') || settings.action;
+				var label = element.data('label') || ''; // optional
+				var value = element.data('value') || 1; // optional
 
 				ga('send', 'event', category, action, label, value);
 			}
-        });
+		});
 
-		function log(message)
-		{
-			if(typeof console === 'object')
-			{
+		function log(message) {
+			if (typeof console === 'object') {
 				console.log(message);
 			}
 		}
-    }
+	}
 }(jQuery));
